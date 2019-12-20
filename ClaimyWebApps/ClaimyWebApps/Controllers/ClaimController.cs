953efef46b77b8a833b48ccd87a9b01791601b26
maximilienviallon <42693169+maximilienviallon@users.noreply.github.com>
2019-12-20 02:56:34 +0100

    added save method to Claims

--- ClaimyWebApps/ClaimyWebApps/Controllers/ClaimController.cs
+++ ClaimyWebApps/ClaimyWebApps/Controllers/ClaimController.cs
@@ -41,5 +41,40 @@ namespace ClaimyWebApps.Controllers
             };
             return View("NewClaim", viewModel);
         }
+        public ActionResult Save(Claim claim)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new ClaimformViewModel
+                {
+                    Claim = claim,
+                    ZipCities = _context.ZipCities.ToList(),
+                    Customers = _context.Customers.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+            if(claim.ID == 0)
+            {
+                _context.Claims.Add(claim);
+            }
+            else
+            {
+                var claimInDb = _context.Claims.Single(c => c.ID == claim.ID);
+                claimInDb.FeeNum = claim.FeeNum;
+                claimInDb.Transgression = claim.Transgression;
+                claimInDb.Remarks = claim.Remarks;
+                claimInDb.LicensePlate = claim.LicensePlate;
+                claimInDb.DriversFirstName = claim.DriversFirstName;
+                claimInDb.Address= claim.Address;
+                claimInDb.Appeal = claim.Appeal;
+                claimInDb.GuardNumber = claim.GuardNumber;
+                claimInDb.Amount = claim.Amount;
+                claimInDb.PaymentInfo = claim.PaymentInfo;
+                claimInDb.Zipcode= claim.Zipcode;
+                claimInDb.CustomerEmail = claim.CustomerEmail;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Claim");
+        }
     }
 }
